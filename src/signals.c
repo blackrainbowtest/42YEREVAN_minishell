9dfe35e3b5b145aafd0bd430678b95a1a46efd20
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-08-24 15:52:58 +0400

    feat: add simple examples for testing

---
+++ src/signals.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 15:20:18 by aramarak          #+#    #+#             */
+/*   Updated: 2025/08/24 15:21:59 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * Ctrl + C обработчик
+ */
+static void	sigint_handler(int signo)
+{
+	(void)signo;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+/**
+ * Установка обработчиков сигналов
+ * Ctrl + \ Игнорировать сигнал
+ * Ctrl + C Перенаправление в функцию sigint_handler
+ */
+void	setup_signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
