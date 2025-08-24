45ba32f593c90d842076fd8a25b0018bfd209735
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-08-24 15:53:05 +0400

    feat: add simple examples for testing

---
+++ src/utils.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 15:33:48 by aramarak          #+#    #+#             */
+/*   Updated: 2025/08/24 15:34:48 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+size_t	ms_env_count(char **envp)
+{
+	size_t	n;
+
+	if (!envp)
+		return (0);
+	n = 0;
+	while (envp[n])
+		n++;
+	return (n);
+}
+
+
+char	**ms_env_dup(char **envp)
+{
+	size_t	n;
+	size_t	i;
+	char	**cp;
+
+	n = ms_env_count(envp);
+	cp = (char **)malloc((n + 1) * sizeof(char *));
+	if (!cp)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < n)
+	{
+		cp[i] = strdup(envp[i]);
+		if (!cp[i])
+		{
+			perror("strdup");
+			while (i > 0)
+				free(cp[--i]);
+			free(cp);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	cp[n] = NULL;
+	return (cp);
+}
+
+void	ms_env_free(char **env)
+{
+	size_t	i;
+
+	if (!env)
+		return ;
+	i = 0;
+	while (env[i])
+		free(env[i++]);
+	free(env);
+}
+
+void	ms_env_print(char **env)
+{
+	size_t	i;
+
+	i = 0;
+	while (env && env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+}
