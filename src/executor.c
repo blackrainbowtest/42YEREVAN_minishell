fb2fc366eef52efecc41fff9aeb8a3f61fbe5aee
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-08-26 20:16:49 +0400

    feat: changed execute function functionality now it use execvp

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 15:38:18 by aramarak          #+#    #+#             */
-/*   Updated: 2025/08/24 15:38:20 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/08/26 19:55:39 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,37 @@
 
 int	execute_command(char **argv, t_shell *sh)
 {
-	size_t	i;
+	pid_t	pid;
+	int		status;
 
 	(void)sh;
 	if (!argv || !argv[0])
 		return (0);
-	printf("[exec stub] tokens:");
-	i = 0;
-	while (argv[i])
-		printf(" {%s}", argv[i++]);
-	printf("\n");
-	return (0);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execvp(argv[0], argv);
+		perror("execvp");
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) < 0)
+		{
+			perror("waitpid");
+			return (1);
+		}
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			return (128 + WTERMSIG(status));
+		else
+			return (1);
+
+	}
 }
