3eadb0a19bb99d5b4cad68245b1f547782781e70
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-09-02 22:24:16 +0400

    feat: changed execvp to execve

--- src/executor.c
+++ src/executor.c
@@ -6,20 +6,17 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 15:38:18 by aramarak          #+#    #+#             */
-/*   Updated: 2025/08/26 19:55:39 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/09/02 22:00:14 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	execute_command(char **argv, t_shell *sh)
+static int	spawn_and_wait(char *path, char **argv, t_shell *sh)
 {
 	pid_t	pid;
 	int		status;
 
-	(void)sh;
-	if (!argv || !argv[0])
-		return (0);
 	pid = fork();
 	if (pid < 0)
 	{
@@ -28,23 +25,37 @@ int	execute_command(char **argv, t_shell *sh)
 	}
 	if (pid == 0)
 	{
-		execvp(argv[0], argv);
-		perror("execvp");
-		exit (EXIT_FAILURE);
+		execve(path, argv, sh->env);
+		perror("execve");
+		_exit (126);
 	}
-	else
+	if (waitpid(pid, &status, 0) < 0)
 	{
-		if (waitpid(pid, &status, 0) < 0)
-		{
-			perror("waitpid");
-			return (1);
-		}
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-		else if (WIFSIGNALED(status))
-			return (128 + WTERMSIG(status));
-		else
-			return (1);
-
+		perror("waitpid");
+		return (1);
 	}
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (1);
+}
+
+int	execute_command(char **argv, t_shell *sh)
+{
+	char	*path;
+	int		code;
+
+	if (!argv || !argv[0])
+		return (0);
+
+	path = find_in_path(argv[0], sh->env);
+	if (!path)
+	{
+		fprintf(stderr, "minishell: %s: command not found\n", argv[0]);
+		return (127);
+	}
+	code = spawn_and_wait(path, argv, sh);
+	free(path);
+	return (code);
 }
