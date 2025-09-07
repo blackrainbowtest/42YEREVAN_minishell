848989d9ae128b089e608bfdbdfbe48a127368f2
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-09-07 04:06:30 +0400

    refactor: update function to LIBFT one

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -25,7 +25,7 @@
  *
  * @return Always returns 0.
  *
- * @note This function writes directly to stdout using printf() and write().
+ * @note This function writes directly to stdout using write().
  */
 int	builtin_echo(char **args)
 {
@@ -34,19 +34,19 @@ int	builtin_echo(char **args)
 
 	i = 1;
 	newline = 1;
-	if (args[1] && strcmp(args[1], "-n") == 0)
+	if (args[1] && ft_strcmp(args[1], "-n") == 0)
 	{
 		newline = 0;
 		i = 2;
 	}
 	while (args[i])
 	{
-		printf("%s", args[i]);
+		write(1, args[i], ft_strlen(args[i]));
 		if (args[i + 1])
-			printf(" ");
+			write(1, " ", 1);
 		i++;
 	}
 	if (newline)
-		printf("\n");
+		write(1, "\n", 1);
 	return (0);
 }
