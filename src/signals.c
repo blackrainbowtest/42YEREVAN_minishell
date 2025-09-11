ec73ee4aff1ea514fd1e9aff2f2715d5e4f6ac8f
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2025-09-11 20:08:42 +0400

    refactor: add Doxygen description

--- src/signals.c
+++ src/signals.c
@@ -6,14 +6,25 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 15:20:18 by aramarak          #+#    #+#             */
-/*   Updated: 2025/09/11 19:55:17 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/09/11 20:08:06 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * Ctrl + C обработчик
+ * @brief Handles the SIGINT signal (Ctrl+C) in the shell.
+ *
+ * This function is registered as a signal handler for SIGINT.
+ * When the user presses Ctrl+C, it moves the cursor to a new line,
+ * clears the current input line, and redisplays the prompt without exiting
+ * the shell. It does not terminate the program.
+ *
+ * @param signo The signal number (ignored in this handler).
+ *
+ * @note This handler is designed to work with GNU readline.
+ * @warning Modifies the readline state; should only be used in the main shell
+ *          input loop.
  */
 static void	sigint_handler(int signo)
 {
@@ -25,9 +36,19 @@ static void	sigint_handler(int signo)
 }
 
 /**
- * Установка обработчиков сигналов
- * Ctrl + \ Игнорировать сигнал
- * Ctrl + C Перенаправление в функцию sigint_handler
+ * @brief Configures signal handling for the shell.
+ *
+ * This function sets up custom signal handlers for the shell. It registers
+ * `sigint_handler` for SIGINT (Ctrl+C) to allow the shell to handle
+ * interrupts gracefully without exiting. It ignores the SIGQUIT signal
+ * (Ctrl+\) to prevent the shell from quitting unexpectedly.
+ *
+ * @return This function does not return a value.
+ *
+ * @note Should be called once during shell initialization, before the main
+ *       input loop starts.
+ * @warning Modifies the process's global signal handlers; calling this
+ *          multiple times may overwrite previous handlers.
  */
 void	setup_signals(void)
 {
