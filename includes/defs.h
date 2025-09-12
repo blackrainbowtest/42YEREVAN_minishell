c9661823254b8361e8ee837d80c7477fb9db2dda
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-09-12 22:28:53 +0400

    feat: update exit

--- includes/defs.h
+++ includes/defs.h
@@ -6,12 +6,14 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 22:37:27 by aramarak          #+#    #+#             */
-/*   Updated: 2025/09/07 22:38:16 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/09/12 22:23:12 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFS_H
 # define DEFS_H
+
+# define DEBUG 1
 
 # define PWD_CRWD "minishell: pwd: cannot retrieve working directory\n"
 # define EXP_AERR "minishell: export: %s: allocation error\n"
--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,11 +6,64 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 19:39:32 by aramarak          #+#    #+#             */
-/*   Updated: 2025/09/12 01:22:51 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/09/12 22:28:30 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+long long	ft_atoll(const char *str)
+{
+	long long	res;
+	int			sign;
+	int			i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+int	is_numeric_arg(char *arg)
+{
+	int	i;
+
+	if (!arg || !*arg)
+		return (0);
+	i = 0;
+	if (arg[i] == '-' || arg[i] == '+')
+		i++;
+	if (!arg[i])
+		return (0);
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	get_exit_status(const char *str)
+{
+	long long	status;
+
+	status = ft_atoll(str);
+	return ((unsigned char)status);
+}
 
 int	builtin_exit(char **argv)
 {
@@ -19,6 +72,18 @@ int	builtin_exit(char **argv)
 	ft_putendl_fd("exit", 1);
 	if (!argv[1])
 		exit(0);
-	status = ft_atoi(argv[1]);
+	if (!is_numeric_arg(argv[1]))
+	{
+		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd(argv[1], 2);
+		ft_putendl_fd(": numeric argument required", 2);
+		exit(255);
+	}
+	if (argv[2])
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		return (1);
+	}
+	status = get_exit_status(argv[1]);
 	exit(status);
 }
