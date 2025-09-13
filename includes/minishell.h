1bf934248bcafe8df6997a9eec03c45aade19984
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-09-13 14:39:55 +0400

    refactor: move commands to parser.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 13:23:38 by aramarak          #+#    #+#             */
-/*   Updated: 2025/09/11 19:54:38 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/09/13 14:32:41 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,13 @@
 # include "defs.h"
 # include "env.h"
 # include "builtins.h"
+# include "parser.h"
 
 // ===== signals =====
 void	setup_signals(void);
 
 // ===== prompt =====
 char	*read_prompt(void);
-
-// ===== parser =====
-// Простая версия: разбивка по пробелам/табам (без кавычек пока)
-char	**parse_input(const char *line);
-void	free_argv(char **argv);
 
 /* path resolver */
 char	*find_in_path(const char *cmd, t_env *env);
