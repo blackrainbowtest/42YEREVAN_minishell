efc2c1c4032bda889ed76d7964944a3c718362ad
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2025-09-20 12:05:29 +0400

    feat: add expand functionality frame

---
+++ includes/expand.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 12:01:56 by aramarak          #+#    #+#             */
+/*   Updated: 2025/09/20 12:03:24 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXPAND_H
+# define EXPAND_H
+
+typedef struct s_cmd	t_cmd;
+typedef struct s_env	t_env;
+/**
+ * @brief Expands environment variables in the command arguments.
+ * This function iterates over the list of commands and their
+ * arguments, replacing any occurrences of environment
+ * variables (e.g., $VAR) with their corresponding
+ */
+void	expand_variables(t_cmd *cmds, t_env *env);
+#endif
