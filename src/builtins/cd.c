8f7cfe07db696213478e1761df9889f4212ed379
Abroyan1 <148694300+Abroyan1@users.noreply.github.com>
2025-09-21 15:45:12 +0400

    cd.c

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -9,57 +9,67 @@
 /*   Updated: 2025/09/07 15:35:19 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minishell.h"
+
+static int	check_cd_args(char **args)
+{
+	int	count;
+
+	count = 0;
+	while (args[count])
+		count++;
+	if (count > 2)
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+static char	*get_cd_target(char **args, t_env **env)
+{
+	if (!args[1])
+	{
+		if (!ft_getenv(*env, "HOME"))
+		{
+			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+			return (NULL);
+		}
+		return (ft_getenv(*env, "HOME"));
+	}
+	if (ft_strcmp(args[1], "-") == 0)
+	{
+		if (!ft_getenv(*env, "OLDPWD"))
+		{
+			ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+			return (NULL);
+		}
+		ft_putendl_fd(ft_getenv(*env, "OLDPWD"), 1);
+		return (ft_getenv(*env, "OLDPWD"));
+	}
+	return (args[1]);
+}
 
 int	builtin_cd(char **args, t_env **env)
 {
-	char	*target;
 	char	cwd[4096];
 	char	*oldpwd;
+	char	*target;
 
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("minishell: cd");
+	if (check_cd_args(args))
 		return (1);
-	}
+	if (!getcwd(cwd, sizeof(cwd)))
+		return (perror("minishell: cd"), 1);
 	oldpwd = ft_strdup(cwd);
-	if (!args[1])
-	{
-		target = ft_getenv(*env, "HOME");
-		if (!target)
-		{
-			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
-			free(oldpwd);
-			return (1);
-		}
-	}
-	else if (ft_strcmp(args[1], "-") == 0)
-	{
-		target = ft_getenv(*env, "OLDPWD");
-		if (!target)
-		{
-			ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
-			free(oldpwd);
-			return (1);
-		}
-		ft_putendl_fd(target, 1);
-	}
-	else
-		target = args[1];
+	target = get_cd_target(args, env);
+	if (!target)
+		return (free(oldpwd), 1);
 	if (chdir(target) != 0)
-	{
-		perror("minishell: cd");
-		free(oldpwd);
-		return (1);
-	}
+		return (perror("minishell: cd"), free(oldpwd), 1);
 	ft_setenv(env, "OLDPWD", oldpwd, 1);
 	free(oldpwd);
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("minishell: cd");
-		return (1);
-	}
+	if (!getcwd(cwd, sizeof(cwd)))
+		return (perror("minishell: cd"), 1);
 	ft_setenv(env, "PWD", cwd, 1);
 	return (0);
 }
