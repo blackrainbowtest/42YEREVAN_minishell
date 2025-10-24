44e263b88f13f4322c13abe3106cc3f534283687
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-10-25 01:07:50 +0400

    refactor: update

--- src/redirections/apply_redirections.c
+++ src/redirections/apply_redirections.c
@@ -33,7 +33,9 @@ static int	apply_single_redir(t_redir *r)
 	fd = open_target_file(r);
 	if (fd < 0)
 	{
-		perror(r->file);
+		ft_putstr_fd("minishell: failed to open heredoc for `", 2);
+		ft_putstr_fd(r->file, 2);
+		ft_putendl_fd("'", 2);
 		return (-1);
 	}
 	if (is_input_redir(r))
