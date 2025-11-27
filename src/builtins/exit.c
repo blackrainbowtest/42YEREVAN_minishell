3a1b2f2952c747d8e1e4dd53f5f1098341fbc13c
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2025-11-27 20:53:50 +0400

    feat: add valgrind checker

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -64,13 +64,22 @@ static int	get_exit_status(const char *str)
 
 int	builtin_exit(char **argv)
 {
+	int	status;
+	int	err_code;
+
 	if (isatty(STDIN_FILENO))
 		ft_putendl_fd("exit", STDERR_FILENO);
 	if (!argv[1])
-		exit(0);
+		clean_and_exit(0);
 	if (!is_numeric_arg(argv[1]))
-		exit(print_minishell_error("exit", argv[1], ERR_NUMERIC, 2));
+	{
+		err_code = print_minishell_error("exit", argv[1], ERR_NUMERIC, 2);
+		clean_and_exit(err_code);
+	}
 	if (argv[2])
 		return (print_minishell_error("exit", NULL, ERR_TOO_MNY_ARGS, 1));
-	exit(last_status(1, get_exit_status(argv[1])));
+	status = get_exit_status(argv[1]);
+	last_status(1, status);
+	clean_and_exit(status);
+	return (status);
 }
